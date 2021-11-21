fbf82c9c1374acfbface55826fbc227d2fb1a56d
Dinesh <dinesh@bournedigital.com.au>
2021-11-21 18:14:55 +1100

    Added code

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,23 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
+
+function addressQuotes(url) {
+  return httpGet(url).then((result) => {
+    if (result.status == 200) {
+      return {
+        "Arnie Quote": JSON.parse(result.body).message,
+      };
+    } else {
+      return {
+        FAILURE: JSON.parse(result.body).message,
+      };
+    }
+  });
+}
 
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
-  // return results;
+  return Promise.all(urls.map((url) => addressQuotes(url)));
 };
-
 module.exports = {
   getArnieQuotes,
 };
